b6c57c4ad588a149a6ba965138d21f9cb52bcf28
Qiyan14 <qiyan14@gmail.com>
2025-02-24 20:53:28 -0500

    Update and rename caret.c to terac.c

--- pkg/terac/src/terac.c
+++ pkg/terac/src/terac.c
@@ -252,7 +252,7 @@ static const R_CMethodDef CEntries[] = {
 	{NULL, NULL, 0}
 };
 
-void R_init_caret(DllInfo *dll)
+void R_init_terac(DllInfo *dll)
 {
 	R_registerRoutines(dll, CEntries, NULL, NULL, NULL);
 	R_useDynamicSymbols(dll, FALSE);
